16177ca0ca2f2e7823372b68d7ac263bed71c086
vijaykumar <vijay.bandaragalla@GSPLK-CPU0249.girmiti.in>
2023-03-30 17:15:11 +0530

    first commit of hibernate project

---
+++ HibernateProject/src/main/java/com/girmiti/firstproject/TestProjectApp/App.java
@@ -0,0 +1,13 @@
+package com.girmiti.firstproject.TestProjectApp;
+
+import java.lang.module.Configuration;
+
+public class App {
+	public static void main(String[] args) {
+		Student st = new Student();
+		st.setUsn(12);
+		st.setName("vijay");
+		st.setMarks(12.4);
+		
+	}
+}
---
+++ HibernateProject/src/main/java/com/girmiti/firstproject/TestProjectApp/Student.java
@@ -0,0 +1,40 @@
+package com.girmiti.firstproject.TestProjectApp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class Student { 
+	@Id
+	private int usn;
+	private String name;
+	private double marks;
+	
+	//getters and setters
+	
+	public int getUsn() {
+		return usn;
+	}
+	public void setUsn(int usn) {
+		this.usn = usn;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getMarks() {
+		return marks;
+	}
+	public void setMarks(double marks) {
+		this.marks = marks;
+	}
+	public Student() {
+	}
+	@Override
+	public String toString() {
+		return "Student [usn=" + usn + ", name=" + name + ", marks=" + marks + "]";
+	}
+	
+	
+}
